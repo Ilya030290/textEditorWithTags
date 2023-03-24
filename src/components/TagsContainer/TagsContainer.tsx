c0026f826fe11b596990ddcebd83e0089090ebd4
Ilya030290 <anoshko90@mail.ru>
2023-03-24 17:45:42 +0300

    feat: add TagsContainer

---
+++ src/components/TagsContainer/TagsContainer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { useAppDispatch } from '../../types/types';
+import { removeTag } from '../../redux/appSlice';
+import { selectAllUniqueTags } from '../../redux/selectors';
+import DeleteIcon from '../../assets/delete.png';
+import styles from './TagsContainer.module.scss';
+
+const TagsContainer = () => {
+  const allUniqueTags = useSelector(selectAllUniqueTags);
+  const dispatch = useAppDispatch();
+
+  const deleteTag = (tag: string) => {
+    dispatch(removeTag(tag));
+  };
+
+  return (
+    <section className={styles.tagsContainer}>
+      {allUniqueTags.length > 0
+        ? allUniqueTags.map((tag, index) => (
+            <div className={styles.tag} key={index}>
+              <div className={styles.tagDescription}>
+                <span className={styles.tagTitle}>{tag}</span>
+                <button className={styles.deleteButton} onClick={() => deleteTag(tag)}>
+                  <img src={DeleteIcon} alt="deleteIcon" />
+                </button>
+              </div>
+            </div>
+          ))
+        : null}
+    </section>
+  );
+};
+
+export default TagsContainer;
