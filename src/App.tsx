ab1e9ca48d1e0bf213cf51b19e534a95343433ed
Ilya030290 <anoshko90@mail.ru>
2023-03-24 17:48:09 +0300

    feat: add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello</div>;
-}
+import Header from './components/Header';
+import SearchBarByTag from './components/SearchBarByTag';
+import NotesList from './components/NotesList';
+import TagsContainer from './components/TagsContainer';
+import styles from './App.module.scss';
+
+const App = () => (
+  <div className={styles.wrapper}>
+    <div className={styles.notesList}>
+      <Header />
+      <SearchBarByTag />
+      <NotesList />
+      <TagsContainer />
+    </div>
+  </div>
+);
 
 export default App;
