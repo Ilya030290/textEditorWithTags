4e2784ffa8d12a0680e9efb35d2f6a3379077fc0
Ilya030290 <anoshko90@mail.ru>
2023-03-25 11:42:35 +0300

    fix: change selectors

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,6 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-import { AppRootStateType, INote } from '../types/types';
+import { AppRootStateType } from '../types/types';
 
 const getTextAreaValue = (state: AppRootStateType): string => state.app.textAreaValue;
 
@@ -9,27 +9,12 @@ export const selectTextAreaValue = createSelector(
   (textAreaValue: string) => textAreaValue
 );
 
-const getTagName = (state: AppRootStateType): string => state.app.searchTagValue;
+const getTagValue = (state: AppRootStateType): string => state.app.searchTagValue;
 
-export const selectTagName = createSelector(
-  getTagName,
+export const selectTagValue = createSelector(
+  getTagValue,
   (searchTagValue: string) => searchTagValue
 );
-
-const getTags = (state: AppRootStateType): string[] => state.app.tags;
-
-export const selectTags = createSelector(getTags, (tags: string[]) => tags);
-
-const getAllUniqueTags = (state: AppRootStateType): string[] => state.app.allUniqueTags;
-
-export const selectAllUniqueTags = createSelector(
-  getAllUniqueTags,
-  (uniqueTags: string[]) => uniqueTags
-);
-
-const getData = (state: AppRootStateType): INote[] => state.app.data;
-
-export const selectData = createSelector(getData, (data: INote[]) => data);
 
 const getJsonData = (state: AppRootStateType): string => state.app.jsonData;
 
